af04784726ce84871cb6c614a61bbe1cbcee1637
lucascovic <lucas.covic@elev.ga.ntig.se>
2020-09-15 14:53:44 +0200

    tränat php

---
+++ kap2/uppgift-2.4.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* PHP version 7
+* @category   Lånekalkylator
+* @author     Karim Ryde <karye.webb@gmail.com>
+* @license    PHP CC
+*/
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Celcius till farenheit<</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        // Ta emot data och variabler
+        $celcius = $_POST["celcius"];
+        $farenheit = $celcius * 9/5 + 32;
+
+        // Skriv ut
+        echo "<p>$celcius&deg; Celcius motsvarar $farenheit&deg; Farenheit</p>";
+        ?>
+    </div>
+</body>
+</html>
